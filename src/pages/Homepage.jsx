cf18b67af19ee5dc9d5314052a09d3e8c687899d
Ansh <anshsahu7705@gmail.com>
2025-05-27 18:48:20 +0530

    Last commit

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import Button from "../components/Button";
 
 function Homepage() {
   const navigate = useNavigate();
   return (
-    <div className="min-h-screen bg-[#F7F8F9] flex items-center justify-center">
-      <div className="w-[375px] h-[812px] bg-white shadow-md flex flex-col justify-end p-6 rounded-md">
+    <div className="min-h-screen bg-[#F7F8F9] pb-1 flex items-center justify-center">
+      <div className="w-[375px] h-[812px] bg-white shadow-md flex flex-col justify-end p-6 rounded-sm border-gray-400">
         <div className="mb-8">
           <h1 className="text-2xl font-semibold text-[#1D2226] mb-2 text-left">
             Welcome to PopX
@@ -17,19 +18,22 @@ function Homepage() {
         </div>
 
         <div className="flex flex-col gap-3">
-          <button
+          <Button
             onClick={() => navigate("/signin")}
             className="w-full bg-[#6C25FF] text-white text-sm font-semibold py-3 rounded-md"
-          >
-            Create Account
-          </button>
+            children='Create Account'
+            type="button"
+          />
+           
+          
 
-          <button
+          <Button
             onClick={() => navigate("/login")}
-            className="w-full bg-[#EEE5FF] text-[#1D2226] text-sm font-semibold py-3 rounded-md"
-          >
-            Already Registered? Login
-          </button>
+            type="button"
+            children='Already Registered? Login'
+            className="w-full bg-purple-200 text-[#1D2226] text-sm font-semibold py-3 rounded-md"
+          />
+           
         </div>
       </div>
     </div>
