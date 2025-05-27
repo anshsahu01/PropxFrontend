be9ccad09fcb07fba19c4435fcff7efb2df6dcbd
Ansh <anshsahu7705@gmail.com>
2025-05-27 19:30:08 +0530

    Resolved import for store.js

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 
-import store from "./Store/store.js";
+import store from "./Store/Store.js";
 import {
   BrowserRouter,
   createBrowserRouter,
